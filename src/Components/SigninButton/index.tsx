9f66f46c47f46c2421ab3e12d69c809de80479bf
venonh2 <brendow-evan@hotmail.com>
2022-01-11 19:15:53 -0300

    feat: auth com github via next

--- src/Components/SigninButton/index.tsx
+++ src/Components/SigninButton/index.tsx
@@ -1,21 +1,30 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 import styles from "./styles.module.scss";
 
 export function SigninButton() {
-  const isUserLoggedIn = true;
+  const { status, data } = useSession();
 
-  return isUserLoggedIn ? (
-    <button className={styles.signInButton}>
-      <FaGithub color="#84d361" />
-      Brendow Evangelista
-      <FiX color="#737388" className={styles.closeIcon} />
-    </button>
-  ) : (
-    <button className={styles.signInButton}>
-      <FaGithub color="#eba417" />
-      Sign in with Github
-    </button>
-  );
+  console.log(status);
+
+  if (status === "authenticated") {
+    return (
+      <button onClick={() => signOut()} className={styles.signInButton}>
+        <FaGithub color="#84d361" />
+        {data.user.name}
+        <FiX color="#737388" className={styles.closeIcon} />
+      </button>
+    );
+  }
+
+  if (status === "unauthenticated" || status === "loading") {
+    return (
+      <button onClick={() => signIn("github")} className={styles.signInButton}>
+        <FaGithub color="#eba417" />
+        Sign in with Github
+      </button>
+    );
+  }
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,16 @@
 import { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
+
 import Header from "../Components/Header";
 
 import "../styles/global.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   );
 }
 
