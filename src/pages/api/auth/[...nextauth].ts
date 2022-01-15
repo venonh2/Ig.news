fb033bb1b12b706dc9c0f949c32d962cf79c1f32
venonh2 <brendow-evan@hotmail.com>
2022-01-15 09:26:32 -0300

    feat: validacao de duplicidade de usuario

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { signIn } from "next-auth/react";
+
+import { query as q } from "faunadb";
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -11,4 +15,30 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  jwt: {
+    secret: process.env.JWT_SECRET,
+  },
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      try {
+        await fauna.query(
+          //q.Create(q.Collection("users"), { data: { email: user.email } })
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index("user_by_email"), q.Casefold(user.email))
+              )
+            ),
+            q.Create(q.Collection("users"), { data: { email: user.email } }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email)))
+          )
+        );
+        return true;
+      } catch (err) {
+        console.log("asasas", err);
+
+        return false;
+      }
+    },
+  },
 });
